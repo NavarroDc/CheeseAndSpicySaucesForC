183e9b969af4e1c890f1f806a16c89a315c38ec3
Dei Dei <angelnavcam321@gmail.com}>
2023-04-06 21:51:39 -0600

    Version, 1

---
+++ assignments.js
@@ -0,0 +1,11 @@
+//Variables 1
+
+let country = "Costa Rica"
+let continent = "America"
+let poppulation = "5M"
+
+console.log(country);
+console.log(continent);
+console.log(poppulation);
+
+//Tipos de datos
---
+++ script.js
@@ -0,0 +1,47 @@
+/*
+let js = "amazing";
+
+40 + 37 + 22 - 18;
+console.log(40 + 37 + 22 - 18);
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Mobiii2";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let numberOfYears = ("19");
+console.log(numberOfYears);
+
+let veronica_lenny = "VL";
+console.log(veronica_lenny);
+*/
+
+/*
+let jajaSiSalió = true;
+console.log(jajaSiSalió);
+
+console.log(typeof true);
+console.log(typeof jajaSiSalió);
+console.log(typeof 43);
+console.log(typeof "Veronica");
+
+jajaSiSalió = "Sep";
+console.log(typeof jajaSiSalió);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023
+console.log(typeof year);*/
+
+let age = 19;
+age = 20;
+
+const birthYear = 2003;
+
+
